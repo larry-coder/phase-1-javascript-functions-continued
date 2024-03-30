eb398c7f905861f85d63e6aafff49dd4c5f5b344
omorut <hillarydavisomorut@gmail.com>
2024-03-30 20:45:29 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// code your solution here
+// test 1
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+console.log(saturdayFun());
+console.log(saturdayFun("bathe my dog"));
+
+// test 2
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;    
+}
+
+console.log(mondayWork());
+console.log(mondayWork("work from home"));
+
+// test 3
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`;
+    }
+}
+
+const encouragingPromptFunction = wrapAdjective("!!!");
+console.log(encouragingPromptFunction("a dedicated programmer"));
+console.log(encouragingPromptFunction());
+
+  
